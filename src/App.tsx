ce4d00eee054a9138bddce816ec6f301b6a447e8
iamnotaturtle <iamnotaturtle@users.noreply.github.com>
2019-10-23 22:55:01 -0400

    feat(app): adds css for num pad'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const numberPad = {
+  1: {
+    label: '1',
+    subLabel: '.,?/',
+  },
+  2: {
+    label: '2',
+    subLabel: 'ABC',
+  },
+  3: {
+    label: '3',
+    subLabel: 'DEF',
+  },
+  4: {
+    label: '4',
+    subLabel: 'GHI',
+  },
+  5: {
+    label: '5',
+    subLabel: 'JKL',
+  },
+  6: {
+    label: '6',
+    subLabel: 'MNO',
+  },
+  7: {
+    label: '7',
+    subLabel: 'PQRS',
+  },
+  8: {
+    label: '8',
+    subLabel: 'TUV',
+  },
+  9: {
+    label: '9',
+    subLabel: 'WXYZ',
+  },
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="phone">
+        <div className="number-pad">
+          <div className="text-box">hey</div>
+          { Object.entries(numberPad).map((index, key) => 
+              <div className="number-key">
+                <div className="label">{index[1].label}</div>
+                <div className="sub-label">{index[1].subLabel}</div>
+              </div>
+            ) 
+          } 
+        </div>
+      </div>
     </div>
   );
 }
