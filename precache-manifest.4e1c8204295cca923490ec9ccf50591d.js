37edb8d67185a571278823bc60ec171be8707779
iamnotaturtle <ygaberman@gmail.com>
2021-06-08 18:50:43 +0000

    Deploying to gh-pages from master 1195738516ebc8f5144578f8e31b4ca1ef69332a

---
+++ precache-manifest.4e1c8204295cca923490ec9ccf50591d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f0452142bed43934b86791b40cc2db",
+    "url": "/t9/index.html"
+  },
+  {
+    "revision": "8023984d2017f3f6e06f",
+    "url": "/t9/static/css/2.06b08db3.chunk.css"
+  },
+  {
+    "revision": "815b80b5fb05a5b773c1",
+    "url": "/t9/static/css/main.10b348b9.chunk.css"
+  },
+  {
+    "revision": "8023984d2017f3f6e06f",
+    "url": "/t9/static/js/2.bfcfe4c6.chunk.js"
+  },
+  {
+    "revision": "815b80b5fb05a5b773c1",
+    "url": "/t9/static/js/main.4ee413d6.chunk.js"
+  },
+  {
+    "revision": "90c24a460090940460de",
+    "url": "/t9/static/js/runtime-main.1f3640df.js"
+  }
+]);
